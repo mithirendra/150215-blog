211dfa06ca42d3de5881fbaa2f32e0f8f3a8fc39
Mithi <mithi98@hotmail.com>
2015-12-02 12:53:44 +0800

    Add login function to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-# Five route methods - GET, POST, UPDATE, PATCH, DELETE
+# Five HTML route methods - GET, POST, UPDATE, PATCH, DELETE
 
 
 # Display new user form
@@ -7,12 +7,14 @@ get '/users/new' do
 	erb :"user/new"
 end
 
+
 # Create new user
 
 post '/users' do
 		user = User.create(name: params[:name], email: params[:email], password: params[:password])
 		redirect "users/#{user.id}"
 end
+
 
 # View user profile
 
@@ -21,12 +23,14 @@ get '/users/:id' do
 	erb :"user/show"
 end
 
+
 # Display user edit form
 
 get '/users/:id/edit' do
 	@user = User.find(params[:id])
 	erb :'user/edit'
 end
+
 
 # Update user
 
@@ -36,6 +40,7 @@ patch '/users/:id' do
 	redirect "/users/#{user.id}"
 end
 
+
 # Delete user
 
 delete '/users/:id' do
@@ -44,10 +49,19 @@ delete '/users/:id' do
 	erb :'static/index' 
 end
 
+
 # View login page
 
 get '/users' do
+	erb :"user/index"
+end
 
+
+# User login
+
+post '/users/login' do
+	user = User.find_by(email: params[:email], password: params[:password])
+	redirect "/users/#{user.id}"
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	validates :email, uniqueness: true
+
 end
